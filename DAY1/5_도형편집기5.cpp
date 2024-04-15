f7f696afb1c4a82195e2b147c185ac75fcee0de8
ecourse <smkang@codenuri.co.kr>
2024-04-15 13:39:00 +0900

    Add files via upload

--- DAY1/5_도형편집기5.cpp
+++ DAY1/5_도형편집기5.cpp
@@ -55,12 +55,24 @@ int main()
 		else if (cmd == 8)
 		{
 			std::cout << "몇번째 도형을 복제 할까요 >> ";
+
 			int k;
 			std::cin >> k;
 
 			// k번째 만들었던 도형을 복제해서 새로운 도형을 생성후에 v 에 추가
 			// 어떻게 해야 할까요 ??
 			// k 번째 만들었던 도형은 어떤 도형일까요 ?
+
+			// 아래 처럼 만들수도 있지만, 아래 코드는 OCP 를 만족할수 없습니다.
+			// => 새로운 도형이 추가되면 아래 코드는 수정(추가)되어야 합니다.
+			if (dynamic_cast<Rect*>(v[k]) != 0)
+			{
+				v.push_back(new Rect( *v[k] ) );
+			}
+			else if (dynamic_cast<Circle*>(v[k]) != 0)
+			{
+				v.push_back(new Circle(*v[k]));
+			}
 		}
 	}
 }
