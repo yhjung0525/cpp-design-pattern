f4eb53b6f0a07f962d45d224f58b3c4a62897b42
ecourse <smkang@codenuri.co.kr>
2024-04-15 16:28:19 +0900

    Add files via upload

--- DAY1/7_단위전략3.cpp
+++ DAY1/7_단위전략3.cpp
@@ -2,6 +2,10 @@
 // => 변하는 것을 다른 클래스로 설계
 // => 인터페이스를 사용해서 교체
 
+// 장점 : 하나의 allocator 를 모든 컨테이너가 사용 가능
+// 단점 : allocate, deallocate 함수가 가상함수 이다. 느리다.
+
+
 // 모든 메모리 할당기 정책 클래스가 지켜야 하는 규칙 설계
 template<typename T>
 struct IAllocator
@@ -16,6 +20,7 @@ class vector
 	IAllocator<T>* ax;
 public:
 	void set_allocator(IAllocator<T>* p) { ax = p; }
+
 	void resize(int n)
 	{
 		T* p = ax->allocate(n);
@@ -23,4 +28,23 @@ public:
 		ax->deallocate(p, n);
 	}
 };
+
+template<typename T>
+class MyAllocator : public IAllocator<T>
+{
+public:
+	T* allocate(int sz) override { return new T[sz]; }
+	void deallocate(T* p, int sz) override { delete[] p; }
+};
+
+int main()
+{
+	vector<int> v;
+
+	MyAllocator ax;
+	v.set_allocator(&ax);
+
+	v.resize(10); //ax 를 사용해서 메모리 할당 
+
+}
 
