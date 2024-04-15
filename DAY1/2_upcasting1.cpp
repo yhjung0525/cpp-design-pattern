032069e05200ed6d1611c07d8f0bb20064035d39
ecourse <smkang@codenuri.co.kr>
2024-04-15 09:25:15 +0900

    Add files via upload

---
+++ DAY1/2_upcasting1.cpp
@@ -0,0 +1,44 @@
+class Animal
+{
+public:
+	int age;
+};
+class Dog : public Animal
+{
+public:
+	int color;
+};
+int main()
+{
+	Dog d;
+	Dog* p1 = &d;		// ok
+//	int* p2 = &d;		// error
+	
+	// 핵심 1. upcasting 개념
+	// => 파생 클래스 객체의 주소를 기반 클래스 포인터에 담을수 있다
+	Animal* p3 = &d;	// ok
+
+	// 핵심 2. 기반 클래스 포인터로는 기반 클래스 멤버만 접근 가능.
+	// => C++ 컴파일러는 컴파일 시간에 "포인터의 타입" 으로만 확인
+	p3->age = 10;   // ok
+	p3->color = 10; // error
+
+
+	// 핵심 3. 파생 클래스 멤버에 접근하려면 
+	// => 포인터를 파생 클래스 타입으로 캐스팅해야 한다.
+	// => 단, 이경우 반드시 p3가 가리키는 곳이 Dog 라는 확신이 있어야 한다.
+	static_cast<Dog*>(p3)->color = 10; // ok
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
