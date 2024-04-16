76ec405c81d15aab80251fbd56c543597d25e842
ecourse <smkang@codenuri.co.kr>
2024-04-16 13:06:55 +0900

    Add files via upload

--- DAY2/4_Adapter2.cpp
+++ DAY2/4_Adapter2.cpp
@@ -35,6 +35,9 @@ public:
 };
 //==========================================
 
+// 클래스 어답터 : 클래스의 인터페이스 변경
+// 객체   어답터 : 객체의   인터페이스 변경
+
 class Text : public Shape, public TextView	
 {
 public:
@@ -45,13 +48,32 @@ public:
 		TextView::show();
 	}
 };
+
+// 아래 클래스가 객체 어답터
+class ObjectAdapter : public Shape
+{
+	TextView* tview;  // 핵심 : 포인터 또는 참조를 사용한 멤버 
+					  // => 외부에서 생성된 객체를 가리키겠다는 의도.
+public:
+
+};
+
+
 int main()
 {
 	std::vector<Shape*> v;
 
-	v.push_back(new Text("hello"));
+	TextView tv("ABCD"); // TextView : 클래스
+						 // tv       : 객체
+						  
+	// 이미 생성된 객체인 tv 를 v에 넣을수 있을까요 ?
+	// 위에서 만든 Text 를 사용해서도 tv 자체를 v에 넣을수는 없습니다.
+	// Text 는 "클래스 어답터" 이므로 
+	// 
+//	v.push_back( &tv ); // error. tv 는 Shape 로 부터 상속받지 않았다
 
-	v[0]->draw();
+
+
 }
 
 
