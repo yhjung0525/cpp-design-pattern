cb32b436797e385cbdac54a3551254bafbae85a4
ecourse <smkang@codenuri.co.kr>
2024-04-16 13:12:19 +0900

    Add files via upload

--- DAY2/4_Adapter2.cpp
+++ DAY2/4_Adapter2.cpp
@@ -55,8 +55,18 @@ class ObjectAdapter : public Shape
 	TextView* tview;  // 핵심 : 포인터 또는 참조를 사용한 멤버 
 					  // => 외부에서 생성된 객체를 가리키겠다는 의도.
 public:
+	ObjectAdapter(TextView* v) : tview(v) {}
 
+	// 도형 편집기 시스템의 요구 조건!!
+	// => TextView 의 show 를 draw 이름으로 사용가능하게
+	// => 클래스(TextView)가 아닌 객체(tview가 가리키는) 의 인터페이스 수정
+	void draw() override
+	{
+		tview->show();
+	}
 };
+
+
 
 
 int main()
@@ -72,7 +82,10 @@ int main()
 	// 
 //	v.push_back( &tv ); // error. tv 는 Shape 로 부터 상속받지 않았다
 
-
+	v.push_back( new ObjectAdapter(&tv) ); // 일치 하지 않은 인터페이스를
+										   // 어답터를 통해서 해결
+								// 220v 에 110v 를 연결하기 위해 "돼지코(젠더)" 를 끼우듯
+											
 
 }
 
