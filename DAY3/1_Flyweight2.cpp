ac77ab19a6edc80dca335455b7ca433ad0b6ea14
ecourse <smkang@codenuri.co.kr>
2024-04-17 08:23:45 +0900

    Add files via upload

--- DAY3/1_Flyweight2.cpp
+++ DAY3/1_Flyweight2.cpp
@@ -15,13 +15,32 @@ public:
 	void Draw() { std::cout << "Draw " << image_url << std::endl; }
 
 	// Image 객체를 생성해 주는 static 멤버 함수
+	// => Image 객체를 생성하는 코드가 한곳 에서만 이루어 진다.
+	
+	// inline static : static 멤버 데이타의 클래스 외부 선언이 없어도 됩니다.(C++17)
+	inline static std::map<std::string, Image*> image_map;
+
 	static Image* Create(const std::string& url)
 	{
-		Image* img = new Image(url);
+		Image* img = nullptr;
+
+		auto p = image_map.find(url);
+
+		if (p == image_map.end())
+		{
+			img = new Image(url);
+			image_map[url] = img;
+		}
+		else
+			img = image_map[url];
 
 		return img;
 	}
 };
+
+
+
+
 
 int main()
 {
