e28c4b44c98fed71b473c9265623435d6e9f3972
ecourse <smkang@codenuri.co.kr>
2024-04-17 10:20:40 +0900

    Add files via upload

---
+++ DAY3/3_추상팩토리2.cpp
@@ -0,0 +1,87 @@
+// 3_추상팩토리 - 147
+#include <iostream>
+
+// 모든 종류의 컨트롤은 공통의 기반 클래스 필요
+struct IButton
+{
+	virtual void Draw() = 0;
+	virtual ~IButton() {}
+};
+struct IEdit
+{
+	virtual void Draw() = 0;
+	virtual ~IEdit() {}
+};
+
+
+struct WinButton : public IButton
+{
+	void Draw() { std::cout << "Draw WinButton" << std::endl; }
+};
+struct WinEdit : public IEdit
+{
+	void Draw() { std::cout << "Draw WinEdit" << std::endl; }
+};
+
+struct OSXButton : public IButton
+{
+	void Draw() { std::cout << "Draw OSXButton" << std::endl; }
+};
+struct OSXEdit : public IEdit
+{
+	void Draw() { std::cout << "Draw OSXButton" << std::endl; }
+};
+//-----------------
+
+// 디자인 패턴 23개 종류에는 "Factory" 라는 패턴은 없습니다
+// => "Abstract Factory" 라는 패턴이 있습니다.
+// => 제품의 군을 만들기 위한 인터페이스를 제공하지만, 어떤 종류의 제품인지는
+//    파생 클래스에서 결정한다.
+class IFactory 
+{
+public:
+	IButton* CreateButton() = 0;
+	IEdit* CreateEdit() = 0;
+	virtual ~IFactory() {}
+};
+
+//  style 별로 각 콘트롤을 만드는 공장
+class WinFactory : public IFactory
+{
+public:
+	IButton* CreateButton() { return new WinButton; }
+	IEdit*   CreateEdit()   { return new WinEdit; }
+	virtual ~WinFactory() {}
+};
+class OSXFactory : public IFactory
+{
+public:
+	IButton* CreateButton() { return new OSXButton; }
+	IEdit*   CreateEdit() { return new OSXEdit; }
+	virtual ~OSXFactory() {}
+};
+
+
+int main(int argc, char** argv)
+{
+	IFactory* factory;
+	if (strcmp(argv[1], "-style:OSX") == 0)
+		factory = new OSXFactory;
+	else
+		factory = new WinFactory;
+
+	// 컨트롤이 필요하면 공장을 사용해서 생성
+	IButton* btn = factory->CreateButton();
+	btn->Draw();
+}
+
+
+
+
+
+
+
+
+
+
+
