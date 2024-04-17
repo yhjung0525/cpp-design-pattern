cb0e61f5962563d397a4e15714335d1dad1040d9
ecourse <smkang@codenuri.co.kr>
2024-04-17 09:02:29 +0900

    Add files via upload

--- DAY3/2_Factory2.cpp
+++ DAY3/2_Factory2.cpp
@@ -13,12 +13,33 @@ class Rect : public Shape
 {
 public:
 	void draw() override { std::cout << "draw Rect" << std::endl; }
+
+	// 자신의 객체를 만드는 static 멤버 함수는 아주 유용하게 활용될수 있습니다.
+	static Shape* Create() { return new Rect; }
 };
+
+// Rect 객체를 생성하는 2가지 방법
+// 1. Shape* p1 = new Rect;
+// 2. Shape* p2 = Rect::Create();
+
+
+
+
+
+
+
+
+
 class Circle : public Shape
 {
 public:
 	void draw() override { std::cout << "draw Circle" << std::endl; }
 };
+
+
+
+
+
 
 class ShapeFactory
 {
